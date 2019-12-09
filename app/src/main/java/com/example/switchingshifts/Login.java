0ce4087b28e504bfe956e55921e5bf3b0bf3646c
EtiReznikov <etirez56@gmail.com>
2019-12-09 12:25:53 +0200

    Error fix

--- app/src/main/java/com/example/switchingshifts/Login.java
+++ app/src/main/java/com/example/switchingshifts/Login.java
@@ -2,24 +2,20 @@ package com.example.switchingshifts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class Login extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 5000;
-=======
-import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
 
->>>>>>> d13b7035958a04c34502f0a8d030712ce9752e3f
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -32,7 +28,7 @@ public class Login extends AppCompatActivity {
 
 
 }
-=======
-    }
-}
->>>>>>> d13b7035958a04c34502f0a8d030712ce9752e3f
+
+
+
+
