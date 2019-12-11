308e2c43c9699b74d89e73d7a9829d4cc5922aa3
EtiReznikov <etirez56@gmail.com>
2019-12-11 18:51:51 +0200

    Manager Screen & Add shift

--- app/src/main/java/com/example/switchingshifts/AddShift.java
+++ app/src/main/java/com/example/switchingshifts/AddShift.java
@@ -12,7 +12,6 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.View;
 import android.app.DatePickerDialog;
-
 import java.util.Calendar;
 
 public class AddShift extends AppCompatActivity {
@@ -22,16 +21,18 @@ public class AddShift extends AppCompatActivity {
     DatePickerDialog dpd;
 
     Spinner s_shift_type;
-    ArrayAdapter<CharSequence> adapter;
+    ArrayAdapter<CharSequence> adapter_shift_type;
+    Spinner s_worker_type;
+    ArrayAdapter<CharSequence> adapter_worker_type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shift);
 
         s_shift_type= (Spinner)findViewById(R.id.spinner_shift_type);
-        adapter= ArrayAdapter.createFromResource(this,R.array.shift_type,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s_shift_type.setAdapter(adapter);
+        adapter_shift_type= ArrayAdapter.createFromResource(this,R.array.shift_type,android.R.layout.simple_spinner_item);
+        adapter_shift_type.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s_shift_type.setAdapter(adapter_shift_type);
         s_shift_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -44,8 +45,24 @@ public class AddShift extends AppCompatActivity {
             }
         });
 
+        s_worker_type= (Spinner)findViewById(R.id.spinner_worker_type);
+        adapter_worker_type= ArrayAdapter.createFromResource(this,R.array.role_type,android.R.layout.simple_spinner_item);
+        adapter_worker_type.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s_worker_type.setAdapter(adapter_worker_type);
+        s_worker_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+"selected", Toast.LENGTH_LONG).show();
+            }
 
-        selectDate = (Button)findViewById(R.id.button_day);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        selectDate = (Button)findViewById(R.id.button_day_A);
         date = (TextView) findViewById(R.id.txt_date);
 
 
