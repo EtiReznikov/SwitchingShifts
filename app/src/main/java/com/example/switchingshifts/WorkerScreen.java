88e80c93aa1d42f592edf43d2d4e42c6d3e50f53
yaaraGoldenberg <yaara812@gmail.com>
2019-12-12 19:04:13 +0200

    Adding WorkerScreen and PersonalDetails

--- app/src/main/java/com/example/switchingshifts/WorkerScreen.java
+++ app/src/main/java/com/example/switchingshifts/WorkerScreen.java
@@ -1,14 +1,56 @@
 package com.example.switchingshifts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 
 public class WorkerScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_screen);
+
+        Toolbar toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
     }
+
+
+
+
+//    public boolean onCreateOptionsMenu(Menu menu){
+//
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id = item.getItemId();
+//        if(id == R.id.myShift){
+//            Intent intent = new Intent(WorkerScreen.this, MyShifts.class);
+//            startActivity(intent);
+//        }
+//        if(id == R.id.messages){
+//            Intent intent = new Intent(WorkerScreen.this, Messages.class);
+//            startActivity(intent);
+//        }
+//        if(id == R.id.personalInfo){
+//            Intent intent = new Intent(WorkerScreen.this, PersonalDetails.class);
+//            startActivity(intent);
+//        }
+//        if(id == R.id.homePage){
+//            Intent intent = new Intent(WorkerScreen.this, WorkerScreen.class);
+//            startActivity(intent);
+//        }
+//        return true;
+//    }
 }
