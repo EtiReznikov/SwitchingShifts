de2144f92c19850fa3aa14006b04a003878c6dec
shaynaor <shaynaor10@gmail.com>
2019-12-13 11:40:11 +0200

    Added logim auth

--- app/src/main/java/com/example/switchingshifts/Login.java
+++ app/src/main/java/com/example/switchingshifts/Login.java
@@ -1,11 +1,10 @@
 package com.example.switchingshifts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -13,26 +12,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
 
-    private EditText loginWorkerNumber, loginPassNumber;
-    private Button loginConnectionButton;
+    private EditText login_email_text, login_password_text;
+    private Button login_connection_button;
+    private FirebaseAuth firebase_auth;
+    private String login_email;
+    private String login_password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebase_auth = FirebaseAuth.getInstance();
 
-        loginWorkerNumber = findViewById(R.id.loginWorkerNumber);
-        loginPassNumber = findViewById(R.id.loginPassNumber);
-        loginConnectionButton = findViewById(R.id.loginConnectionButton);
+        login_email_text = findViewById(R.id.login_email);
+        login_password_text = findViewById(R.id.login_password);
+        login_connection_button = findViewById(R.id.loginConnectionButton);
 
-        loginConnectionButton.setEnabled(false);
+        login_connection_button.setEnabled(false);
 
-        loginWorkerNumber.addTextChangedListener(loginTextWatcher);
-        loginPassNumber.addTextChangedListener(loginTextWatcher);
+        login_email_text.addTextChangedListener(loginTextWatcher);
+        login_password_text.addTextChangedListener(loginTextWatcher);
         Toast.makeText(Login.this, "Firebase Connection Success", Toast.LENGTH_LONG).show();
 
     }
@@ -45,18 +53,29 @@ public class Login extends AppCompatActivity {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String workerNumberInput = loginWorkerNumber.getText().toString().trim();
-            String pessNumberInput = loginPassNumber.getText().toString().trim();
-            if(!workerNumberInput.isEmpty() && !pessNumberInput.isEmpty())
-            loginConnectionButton.setEnabled(true);
-            loginConnectionButton.setOnClickListener(
-                    new Button.OnClickListener(){
-                        @Override
-                        public void onClick(View v) {
-                            startActivity(new Intent(Login.this, ChangePass.class));
-                        }
-                    }
-            );
+            login_email = login_email_text.getText().toString().trim();
+            login_password = login_password_text.getText().toString().trim();
+            if(!login_email.isEmpty() && !login_password.isEmpty())
+                login_connection_button.setEnabled(true);
+            login_connection_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    firebase_auth.signInWithEmailAndPassword(login_email, login_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Login.this,"התחברת בהצלחה", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(Login.this,"האימייל או הסיסמא אינם תקינים", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            });
+
+
         }
 
         @Override
