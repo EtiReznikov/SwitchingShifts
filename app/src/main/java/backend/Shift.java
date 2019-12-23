43d3f63a3aef5c1754785eec6b299b82000380e0
EtiReznikov <etirez56@gmail.com>
2019-12-23 12:11:41 +0200

    Shift class

--- app/src/main/java/backend/Shift.java
+++ app/src/main/java/backend/Shift.java
@@ -1,4 +1,42 @@
 package backend;
 
 public class Shift {
+    /* private data members */
+    private String date;
+    private String role;
+
+    public Shift() {
+        this.date="";
+        this.role="";
+    }
+
+    public Shift(Shift s) {
+        this.date=s.date;
+        this.role=s.role;
+    }
+    public Shift(String date, String role) {
+        this.date=date;
+        this.role=role;
+
+    }
+
+    /* getters and setters*/
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+
 }
